ff974d6375a4358c113420133bb13b3ec6f473b7
xmk <xmk.hg28@qq.com>
2025-05-23 18:18:58 +0800

    更改body的mr

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={inter.className} style={{ marginRight: '0px !important' }}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
